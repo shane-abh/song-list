6b6527d001b9556d527b43d79e0ac70d77a01127
Shane <shaneabh777@gmail.com>
2023-04-20 16:04:04 +0400

    Resized input box

--- src/App.js
+++ src/App.js
@@ -1,58 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import SearchBox from './components/SearchBox';
+import "./App.css";
+import SearchBox from "./components/SearchBox";
 import React, { useState } from "react";
-import scaleA, { minor_chords } from "./data"
-import SongList from './components/Allsongs';
-import { Routes, Route } from 'react-router-dom';
-import { chordSongs } from './data';
-
+import scaleA, { minor_chords } from "./data";
+import SongList from "./components/Allsongs";
+import { Routes, Route } from "react-router-dom";
+import { chordSongs } from "./data";
 
 const App = () => {
-  // const [searchValue, setSearchValue] = useState("");
-  // const [data, setData] = useState(songdata);
-
-  // const handleSearch = event => {
-  //   setSearchValue(event.target.value);
-  // };
-
-  // const filteredData = data.filter(
-  //   item => item.name.toLowerCase().includes(searchValue.toLowerCase())
-  // );
   console.log(minor_chords.Cm);
   return (
-    // <SearchBox scale="A"/>
     <div>
       <div>
-       
         <Routes>
-          <Route path="song-list/" element={<SongList/>} />
+          <Route path="song-list/" element={<SongList />} />
           {/* Major Chord routes */}
-          <Route path="/A" element={<SearchBox scale="A" songdata={chordSongs.A}/>} />
-          <Route path="/B" element={<SearchBox scale="B" songdata={chordSongs.B}/>} />
-          <Route path="/C" element={<SearchBox scale="C" songdata={chordSongs.C}/>} />
-          <Route path="/D" element={<SearchBox scale="D" songdata={chordSongs.D}/>} />
-          <Route path="/E" element={<SearchBox scale="E" songdata={chordSongs.E}/>} />
-          <Route path="/F" element={<SearchBox scale="F" songdata={chordSongs.F}/>} />
-          <Route path="/G" element={<SearchBox scale="G" songdata={chordSongs.G}/>} />
+          <Route
+            path="/A"
+            element={<SearchBox scale="A" songdata={chordSongs.A} />}
+          />
+          <Route
+            path="/B"
+            element={<SearchBox scale="B" songdata={chordSongs.B} />}
+          />
+          <Route
+            path="/C"
+            element={<SearchBox scale="C" songdata={chordSongs.C} />}
+          />
+          <Route
+            path="/D"
+            element={<SearchBox scale="D" songdata={chordSongs.D} />}
+          />
+          <Route
+            path="/E"
+            element={<SearchBox scale="E" songdata={chordSongs.E} />}
+          />
+          <Route
+            path="/F"
+            element={<SearchBox scale="F" songdata={chordSongs.F} />}
+          />
+          <Route
+            path="/G"
+            element={<SearchBox scale="G" songdata={chordSongs.G} />}
+          />
 
           {/* Minor chord routes */}
-          <Route path="/Cm" element={<SearchBox scale="Cm" songdata={minor_chords.Cm}/>} />
-          <Route path="/Dm" element={<SearchBox scale="Dm" songdata={minor_chords.Dm}/>} />
-          <Route path="/Em" element={<SearchBox scale="Em" songdata={minor_chords.Em}/>} />
-          <Route path="/Fm" element={<SearchBox scale="Fm" songdata={minor_chords.Fm}/>} />
-          <Route path="/Gm" element={<SearchBox scale="Gm" songdata={minor_chords.Gm}/>} />
-      </Routes>
-      {/* other routes */}
-    
-      
+          <Route
+            path="/Cm"
+            element={<SearchBox scale="Cm" songdata={minor_chords.Cm} />}
+          />
+          <Route
+            path="/Dm"
+            element={<SearchBox scale="Dm" songdata={minor_chords.Dm} />}
+          />
+          <Route
+            path="/Em"
+            element={<SearchBox scale="Em" songdata={minor_chords.Em} />}
+          />
+          <Route
+            path="/Fm"
+            element={<SearchBox scale="Fm" songdata={minor_chords.Fm} />}
+          />
+          <Route
+            path="/Gm"
+            element={<SearchBox scale="Gm" songdata={minor_chords.Gm} />}
+          />
+        </Routes>
+        {/* other routes */}
+      </div>
     </div>
-  
-     
-      {/* <SongList/> */}
-    </div>
-    
-);
+  );
 };
 
 export default App;
